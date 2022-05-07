4429699898bdf96d2989c9d216473e4b3801faab
Nioni Artha Putri <nioniarthaputri@gmail.com>
2022-05-07 13:50:45 +0700

    part 3 important file

--- home.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-  <h1>Home Page</h1>
-@endsection
--- movies.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-  <h1>Movies Page</h1>
-@endsection
